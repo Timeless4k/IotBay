3ea6788cb77ef179555c020ac878f95f1941dcd3
William Herd <williamherd25@gmail.com>
2024-03-25 23:42:45 +1100

    Java application setup, will need to touch the pom perhaps

--- src/main/java/user.java
+++ src/main/java/user.java
@@ -1,3 +1,8 @@
+// in properties/mysql-build-properties.xml values DB.URL/DB.USER/DB.PASSWORD can be used to hardcode db values
+// jdbc:sqlserver://;servername=server_name;encrypt=true;integratedSecurity=true;authenticationScheme=JavaKerberos
+//  - can also be used to set while running? mainly just placeholders
+
+import java.sql.*; //Java sql tools and JDBC
 public class user {
     
 }
