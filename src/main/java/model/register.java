aee1273ab427210d77990d1b25be71bd63e21145
solgrace <solibungrace@gmail.com>
2024-03-29 01:27:28 +1100

    Fixed 'register.java'

--- src/main/java/model/register.java
+++ src/main/java/model/register.java
@@ -1,4 +1,5 @@
 package model;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,48 +13,38 @@ public class register extends HttpServlet {
     
     // This line defines a method named doPost that handles HTTP POST requests in a servlet
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
+        // Retrieve form data
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String firstName = request.getParameter("firstName");
+        String middleName = request.getParameter("middleName");
+        String lastName = request.getParameter("lastName");
+        String birthDate = request.getParameter("birthDate");
+        String mobilePhone = request.getParameter("mobilePhone");   
+        String gender = request.getParameter("gender");   
+        String date = request.getParameter("date");
 
-            // Retrieve form data
-            String email = request.getParameter("email");
-            String password = request.getParameter("password");
-            String firstName = request.getParameter("firstName");
-            String middleName = request.getParameter("middleName");
-            String lastName = request.getParameter("lastName");
-            String birthDate = request.getParameter("birthDate");
-            String mobilePhone = request.getParameter("mobilePhone");   
-            String gender = request.getParameter("gender");   
-            String date = request.getParameter("date");
+        // Create a User instance and set its properties
+        user user = new user();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setFirstName(firstName);
+        user.setMiddleName(middleName);
+        user.setLastName(lastName);
+        user.setBirthDate(birthDate);
+        user.setMobilePhone(mobilePhone);
 
-            // Create a User instance and set its properties
-            user user = new user();
-            user.setEmail(email);
-            user.setPassword(password);
-            user.setFirstName(firstName);
-            user.setMiddleName(middleName);
-            user.setLastName(lastName);
-            user.setBirthDate(birthDate);
-            user.setMobilePhone(mobilePhone);
-
-            user.setGender(gender);
-            user.setCreationDate(date);
-            user.setuType("Customer");
-            //user.genID(); // Possible future implement 
+        user.setGender(gender);
+        user.setCreationDate(date);
+        user.setuType("Customer");
+        //user.genID(); // Possible future implement 
 
 
-            // Create a session to store user data
-            HttpSession session = request.getSession();
-            session.setAttribute("user", user);
+        // Create a session to store user data
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
 
-            // Redirect user to welcome page 
-            response.sendRedirect("welcome.jsp");
-
-        } catch (ServletException e) {
-            e.printStackTrace(); // Log ServletException
-            // Handle ServletException
-        } catch (IOException e) {
-            e.printStackTrace(); // Log IOException
-            // Handle IOException
-        }
+        // Redirect user to welcome page 
+        response.sendRedirect("welcome.jsp");
     }
 }
