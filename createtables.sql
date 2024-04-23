1470362e7d3bf8b84865390b5e1befa79a251773
William Herd <williamherd25@gmail.com>
2024-04-23 16:23:55 +1000

    datatype fixes

--- createtables.sql
+++ createtables.sql
@@ -3,33 +3,33 @@ CREATE schema IotBay;
 USE IotBay;
 
 CREATE TABLE User(
-    UserID int(10) primary key,
+    UserID BIGINT primary key,
     UserFirstName varchar(50),
     UserMiddleName varchar(50),
     UserLastName varchar(50),
-    UserType varchar(10), --could use a enum value
+    UserType varchar(11), -- could use a enum value
     UserEmail varchar(255),
-    UserPhone int(10),
-    UserGender varchar(10), -- could use a enum type to restrict inputs
+    UserPhone BIGINT,
+    UserGender varchar(11), -- could use a enum type to restrict inputs
     PasswordHash varchar(64),
     UserCreationDate datetime,
     ActivationFlag boolean,
-    VerificationCode varchar(10),
+    VerificationCode varchar(11)
 );
 
 CREATE TABLE ProductData(
-    ProductID int(10) primary key,
+    ProductID BIGINT primary key,
     ProductName varchar(50),
     ProductStatus varchar(20),
-    ProductReleaseDate varchar(10),
-    ProductStockLevel int(10),
+    ProductReleaseDate varchar(11),
+    ProductStockLevel BIGINT,
     ProductDescription varchar(50),
     ProductType varchar(50)
 );
 
 CREATE TABLE AccessData(
-    LogID int(10),
-    UserID int(10),
+    LogID BIGINT,
+    UserID BIGINT,
     LoginTime datetime,
     LogoutTime datetime,
     primary key(LogID),
@@ -37,44 +37,44 @@ CREATE TABLE AccessData(
 );
 
 CREATE TABLE CardInformation(
-    CardID int(10),
-    CardNumber int(10),
+    CardID BIGINT,
+    CardNumber BIGINT,
     CardHolderName varchar(128),
     CardExpiry date,
     CardCVV int(3),
-    UserID int(10),
+    UserID BIGINT,
     primary key(CardID),
     foreign key(UserID) references User(UserID)
 );
 
 CREATE TABLE Payments(
-    PaymentID int(10),
+    PaymentID BIGINT,
     PaymentAmount decimal(13,2),
     PaymentMethod varchar(50),
     PaymentDate datetime,
-    PaymentStatus varchar(20), --ahhhh there are errors in the report in regards to type
-    CardID int(10),
+    PaymentStatus varchar(20), -- ahhhh there are errors in the report in regards to type
+    CardID BIGINT,
     primary key(PaymentID),
     foreign key(CardID) references CardInformation(CardID)
 );
 
 CREATE TABLE ShipmentData(
-    ShipmentID int(10),
-    ShipmentAddress varchar(100),
+    ShipmentID BIGINT,
+    ShipmentAddress varchar(110),
     ShipmentType varchar(30),
-    ShipmentTrackingNumber varchar(10),
+    ShipmentTrackingNumber varchar(11),
     ShipmentExpectedDate datetime,
     primary key(ShipmentID)
 );
 
-CREATE TABLE Order(
-    OrderID int(10),
+CREATE TABLE Orders(
+    OrderID BIGINT,
     OrderDate datetime,
     OrderStatus varchar(20),
     OrderDeliveryStatus varchar(20),
-    PaymentID int(10),
-    ShippingID int(10),
-    UserID int(10),
+    PaymentID BIGINT,
+    ShippingID BIGINT,
+    UserID BIGINT,
     primary key(OrderID),
     foreign key(UserID) references User(UserID),
     foreign key(PaymentID) references Payments(PaymentID),
@@ -82,11 +82,11 @@ CREATE TABLE Order(
 );
 
 CREATE TABLE OrderLineItem(
-    OrderID int(10),
-    ProductID int(10),
+    OrderID BIGINT,
+    ProductID BIGINT,
     OrderAmount int(99),
     primary key(OrderID, ProductID),
-    foreign key(OrderID) references Order(OrderID),
+    foreign key(OrderID) references Orders(OrderID),
     foreign key(ProductID) references ProductData(ProductID)
 );
 
@@ -106,10 +106,7 @@ insert into user values(
     'A4DD5658EC0219465B705EA7C7435D9786A3C66D4F448CABD7488DABCEAFB699',
     '2024-01-01 11:11:11',
     true,
-    6111111111, -- logid
-    8111111111, -- cardid
-    3111111111, -- orderid
-    '2FA111', -- 2fa code
+    '2FA111' -- 2fa code
 );
 
 insert into user values(
@@ -124,10 +121,7 @@ insert into user values(
     'A4DD5658EC0219465B705EA7C7435D9786A3C66D4F448CABD7488DABCEAFB699',
     '2024-01-01 12:35:12',
     true,
-    6222222222, -- logid
-    8222222222, -- cardid
-    3222222222, -- orderid
-    '2FA222', -- 2fa code
+    '2FA222' -- 2fa code
 );
 
 insert into user values(
@@ -142,10 +136,7 @@ insert into user values(
     'A4DD5658EC0219465B705EA7C7435D9786A3C66D4F448CABD7488DABCEAFB699',
     '2024-01-01 11:11:11',
     true,
-    6333333333, -- logid
-    8333333333, -- cardid
-    3333333333, -- orderid
-    '2FA333', -- 2fa code
+    '2FA333' -- 2fa code
 );
 
 insert into user values(
@@ -160,10 +151,7 @@ insert into user values(
     'A4DD5658EC0219465B705EA7C7435D9786A3C66D4F448CABD7488DABCEAFB699',
     '2024-01-01 11:11:11',
     true,
-    6444444444, -- logid
-    8444444444, -- cardid
-    3444444444, -- orderid
-    '2FA444', -- 2fa code
+    '2FA444' -- 2fa code
 );
 
 insert into user values(
@@ -177,10 +165,7 @@ insert into user values(
     'Male',
     'A4DD5658EC0219465B705EA7C7435D9786A3C66D4F448CABD7488DABCEAFB699',
     '2024-01-01 11:11:11',
-    False,  --requested his data be removed due to gdpr regulations
-    6555555555, -- logid
-    8555555555, -- cardid
-    3555555555, -- orderid
-    '2FA444', -- 2fa code
+    False,  -- requested his data be removed due to gdpr regulations
+    '2FA444' -- 2fa code
 );
 
