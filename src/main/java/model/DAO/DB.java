e5092b8e7be81def03b5bd40cf3eec2201c500f8
William Herd <williamherd25@gmail.com>
2024-04-30 14:24:20 +1000

    DBconnection reqs

--- src/main/java/model/DAO/DB.java
+++ src/main/java/model/DAO/DB.java
@@ -1,8 +1,11 @@
 package model.DAO;
 
-public class DB {
+import java.sql.Connection;
+public abstract class DB {
     protected String dbschema = "IotBay";
     protected String dbuser = "root";
     protected String dbpass = "Password123456";
     protected String driver = "com.mysql.cj.jdbc.Driver";
+    protected String dbURL = "jdbc:mysql://localhost:3306/";
+    protected Connection conn;
 }
--- src/main/java/model/DAO/DBConnector.java
+++ src/main/java/model/DAO/DBConnector.java
@@ -1,12 +1,33 @@
 package model.DAO;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Properties;
 public class DBConnector extends DB {
-    public DBConnector() throws ClassNotFoundException, SQLException {
-        Class.forName(driver);
 
-        Properties dbProperties = new Properties();
-        dbProperties.put("user")
-    }
+	public DBConnector() throws ClassNotFoundException, SQLException {
+		Class.forName(driver);
+
+		Properties dbProperties = new Properties();
+		dbProperties.put("user", dbuser);
+		dbProperties.put("password", dbpass);
+		dbProperties.put("allowPublicKeyRetrieval", "true");
+		dbProperties.put("useSSL", "false");
+
+		try {
+			conn = DriverManager.getConnection(dbURL + dbschema, dbProperties);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection openConnection() {
+		return this.conn;
+	}
+
+	public void closeConnection() throws SQLException {
+		this.conn.close();
+	}
 }
