0b2376c648efd9cf3eafd5091c7ab970aa483a0e
William Herd <williamherd25@gmail.com>
2024-05-03 01:20:33 +1000

    servlett mappings

--- src/main/java/Controller/ProductServlet.java
+++ src/main/java/Controller/ProductServlet.java
@@ -12,6 +12,6 @@ public class ProductServlet extends HttpServlet{
     
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        System.out.println("WORKIGNS");
     }
 }
