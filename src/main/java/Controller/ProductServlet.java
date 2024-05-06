c7f1c53ae63b46f942ddab5e73acb0f65be98d00
William Herd <williamherd25@gmail.com>
2024-05-06 13:50:23 +1000

    fordemo

--- src/main/java/Controller/ProductServlet.java
+++ src/main/java/Controller/ProductServlet.java
@@ -9,10 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import model.DAO.productDAO;
 public class ProductServlet extends HttpServlet{
-    
+    private Connection conn;
+    private productDAO PDAO;
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+		HttpSession session = request.getSession();
+        conn = (Connection) session.getAttribute("acticonn");
+        try{
+            PDAO = new productDAO(conn);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        
         
     }
 }
